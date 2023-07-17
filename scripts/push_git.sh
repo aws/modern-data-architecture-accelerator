6501bb89d7b47adf98f43d0054dd8e6e1fa159b0
ndpric <ndpric@amazon.com>
2023-07-17 13:38:16 -0400

    Update CI

--- scripts/push_git.sh
+++ scripts/push_git.sh
@@ -8,13 +8,13 @@ PUBLISHED_VERSION=`cat lerna.json | jq -r .version`
 git commit -a -m "Version $PUBLISHED_VERSION"
 git tag -a $PUBLISHED_VERSION -m "Version $PUBLISHED_VERSION"
 
-#Push published version to prerelease repo. This will be used to track the latest version for next publish
+#Push published version to CAEF Dev repo. This will be used to track the latest version for next publish
 git remote add prerelease_publish "https://gitlab-ci-token:$CI_GROUP_TOKEN@$CI_SERVER_HOST/${CI_PROJECT_PATH}.git/"
 git fetch prerelease_publish
 git push prerelease_publish
 git push prerelease_publish --tags
 
-#Push published version to release repo. This will be used to track the latest version for next publish
+#Push published version to CAEF Release repo. This will become the public version
 git remote add release_publish "https://gitlab-ci-token:$CI_GROUP_TOKEN@$CI_SERVER_HOST/${CAEF_RELEASE_PROJECT_PATH}.git/"
 git fetch release_publish
 git push release_publish
