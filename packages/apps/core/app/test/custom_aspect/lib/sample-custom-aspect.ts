708c640b2340d06388f9e16286da56d097552bac
CICD <ci@example.com>
2023-12-01 18:37:25 +0000

    Version 0.40.0

--- packages/apps/core/app/test/custom_aspect/lib/sample-custom-aspect.ts
+++ packages/apps/core/app/test/custom_aspect/lib/sample-custom-aspect.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { IAspect } from "aws-cdk-lib";
 
 import { IConstruct } from "constructs";
