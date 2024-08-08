c536d8a39527af96abe2bcc25783192dab3fb6ec
Andrew Price <ndpric@amazon.com>
2024-08-08 17:29:23 +0000

    Resolve "Update CI/CD Credentials to use credentials vendor"

--- scripts/push_git_release.sh
+++ scripts/push_git_release.sh
@@ -8,12 +8,12 @@ PUBLISHED_VERSION=`jq -r .version < lerna.json `
 git commit -a -m "Version $PUBLISHED_VERSION"
 git tag -a $PUBLISHED_VERSION -m "Version $PUBLISHED_VERSION"
 
-export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
-$(aws sts assume-role \
---role-arn $CAEF_DELIVERY_CODE_COMMIT_ROLE_ARN \
---role-session-name MdaaDeliveryCodeCommit \
---query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
---output text))
+# export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
+# $(aws sts assume-role \
+# --role-arn $CAEF_DELIVERY_CODE_COMMIT_ROLE_ARN \
+# --role-session-name MdaaDeliveryCodeCommit \
+# --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+# --output text))
 
 #Push published version to CAEF Release repo. This will become the public version.
 git remote add release_publish $CAEF_DELIVERY_CODE_COMMIT_REPO
