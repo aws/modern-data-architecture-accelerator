ca5b761f9c2357d5b9b033c4d741aeedc0d8eb22
John Reynolds <johreyn@amazon.com>
2025-09-22 17:00:31 +0000

    fix: docker commands are failing the feature_merge_smoke_test job

--- packages/constructs/L2/lambda-constructs/src/scripts/build_layer.sh
+++ packages/constructs/L2/lambda-constructs/src/scripts/build_layer.sh
@@ -3,4 +3,4 @@ TEMP_DIR=$1
 PYTHON_VERSION=$2
 cd $TEMP_DIR
 mkdir -p python/ && \
-    "pip$PYTHON_VERSION" install --no-cache-dir --platform manylinux2014_x86_64 --only-binary=:all: -r requirements.txt -t python
+    pip3 install --no-cache-dir --platform manylinux2014_x86_64 --only-binary=:all: -r requirements.txt -t python
