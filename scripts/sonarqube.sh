f958ec18a076e9150ce490054d7db47fc7999227
John Reynolds <johreyn@amazon.com>
2026-03-16 16:13:30 +0000

    chore: update sonarqube gitlab ci jobs

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -5,14 +5,11 @@ echo "Merging coverage reports"
 python3 ./scripts/mergelcov.py
 
 echo "Running Sonar Scanner"
-export SONAR_SCANNER_OPTS="-Xmx1024m"
+export SONAR_SCANNER_JAVA_OPTS="-Xmx1024m"
 sonar-scanner \
-  -Dsonar.projectKey=${CI_COMMIT_REF_SLUG} \
+  -Dsonar.projectKey=${SONAR_PROJECT_KEY:-${CI_PROJECT_PATH_SLUG}} \
   -Dsonar.javascript.lcov.reportPaths=./coverage/merged_lcov.info \
   -Dsonar.qualitygate.wait=true \
   -Dsonar.host.url=${SONAR_SERVER} \
-  -Dsonar.login=${SONAR_LOGIN} \
+  -Dsonar.token=${SONAR_LOGIN} \
   -Dsonar.sourceEncoding=utf-8
-
-
-
