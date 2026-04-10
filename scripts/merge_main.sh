d022801f2c38656965604785f86d25e46642feef
Andrew Price <ndpric@amazon.com>
2026-04-10 11:25:37 +0000

    chore: Make non-merge feature pipelines optional

--- scripts/merge_main.sh
+++ scripts/merge_main.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
+
+if [ "$RUN_FEATURE_PIPELINE" = "true" ]; then
+  echo "ERROR: RUN_FEATURE_PIPELINE is set to 'true'."
+  echo "Set it back to 'false' before merging to main."
+  exit 1
+fi
+
 git checkout "$CI_COMMIT_REF_NAME"
 
 git fetch origin
