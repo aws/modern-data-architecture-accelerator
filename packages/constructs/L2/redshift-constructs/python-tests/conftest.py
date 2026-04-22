3d7ec6ba759a34af4c2baee88c583e29e8071264
Vladimir Nazarov <vlnzrv@amazon.co.uk>
2026-04-22 15:53:01 +0100

    Fix: redshift-constructs: python tets: mandatory aws region set

--- packages/constructs/L2/redshift-constructs/python-tests/conftest.py
+++ packages/constructs/L2/redshift-constructs/python-tests/conftest.py
@@ -1,5 +1,9 @@
 import sys
 import os
 
+# Set a default region so the module-level boto3.client() call in snapshot_copy.py
+# does not fail during import (before mocks are in place)
+os.environ.setdefault('AWS_DEFAULT_REGION', 'us-east-1')
+
 # Add the Lambda source to the path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src', 'lambda', 'snapshot_copy'))
