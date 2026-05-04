601ca282d7129a6c3407321c477d84815f9ef582
John Reynolds <johreyn@amazon.com>
2026-05-04 20:30:13 +0000

    fix: replace t-shirt size tags with Kubernetes resource variables and disable CodeArtifact login

--- scripts/build/npm_install_repo.sh
+++ scripts/build/npm_install_repo.sh
@@ -7,8 +7,5 @@ echo "NPM version (before): $(npm --version)"
 # Suppress WASI experimental warnings
 export NODE_NO_WARNINGS=1
 
-#Login to prerelease CodeArtifact to pull latest prerelease packages for build
-aws codeartifact login --tool npm --repository $MDAA_CODEARTIFACT_PRERELEASE_NPM_REPO --domain $MDAA_CODEARTIFACT_PRERELEASE_DOMAIN --domain-owner $MDAA_CODEARTIFACT_PRERELEASE_ACCOUNT --namespace '@aws-mdaa' --region us-east-1
-
 #Bootstrap and build packages
 npm ci
